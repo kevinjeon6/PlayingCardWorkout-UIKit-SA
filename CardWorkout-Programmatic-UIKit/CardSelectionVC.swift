cf0331474dc4b7f3a871ce674fa4c88c712dfb8d
Kevin <kevin_jeon@icloud.com>
2023-02-14 23:58:51 -0500

    Delete pre-made storyboard and view controller to make app programmatically Create new view controller called CardSelectionVC which is the root view controller Provide CardSelectionVC a background color just to display it on the simulator

--- CardWorkout-Programmatic-UIKit/CardSelectionVC.swift
+++ CardWorkout-Programmatic-UIKit/CardSelectionVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CardSelectionVC.swift
 //  CardWorkout-Programmatic-UIKit
 //
 //  Created by Kevin Mattocks on 2/14/23.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CardSelectionVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .blue
     }
+    
 
 
 }
-
