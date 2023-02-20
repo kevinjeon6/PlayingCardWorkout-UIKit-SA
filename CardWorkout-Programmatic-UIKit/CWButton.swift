86653e433a12d36269598f11fc085a58081e62e5
Kevin <kevin_jeon@icloud.com>
2023-02-19 22:02:22 -0500

    Add configure to the initializer of CWButton that contains the backgroundColor and title Add Stop button, reset button, and rules button with programmatic constraints. Order matters so you can pin it to the appropriate view

--- CardWorkout-Programmatic-UIKit/CWButton.swift
+++ CardWorkout-Programmatic-UIKit/CWButton.swift
@@ -26,6 +26,7 @@ class CWButton: UIButton {
         super.init(frame: .zero)
         self.backgroundColor = backgroundColor
         setTitle(title, for: .normal)
+        configure()
     }
     
     
--- CardWorkout-Programmatic-UIKit/CardSelectionVC.swift
+++ CardWorkout-Programmatic-UIKit/CardSelectionVC.swift
@@ -28,6 +28,9 @@ class CardSelectionVC: UIViewController {
     
     func configureUI() {
         configureCardImageView()
+        configureStopButton()
+        configureResetButton()
+        configureRulesButton()
     }
     
     
@@ -52,6 +55,53 @@ class CardSelectionVC: UIViewController {
             cardImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -75)
         ])
     }
+    
+    
+    
+    // MARK: The order matters. Need to create the card image view so that you can pin the stop button to the bottom of the card image view
+    // MARK: Stop button
+    func configureStopButton() {
+        view.addSubview(stopButton)
+        //Dont need to add the TAMIC because the CWButton already has it in the configure
+        
+        //Set Constraints
+        NSLayoutConstraint.activate([
+            stopButton.widthAnchor.constraint(equalToConstant: 260),
+            stopButton.heightAnchor.constraint(equalToConstant: 50),
+            stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            //TopAnchor- top of the stop button. And we are pinning it to the bottom of the card image view
+            stopButton.topAnchor.constraint(equalTo: cardImageView.bottomAnchor, constant: 30)
+        ])
+    }
+    
+    
+    // MARK: Reset Button
+    func configureResetButton(){
+        view.addSubview(resetButton)
+        
+        NSLayoutConstraint.activate([
+            resetButton.widthAnchor.constraint(equalToConstant: 115),
+            resetButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            //Lines up the reset button to the stop button with the anchor being leading. Meaning aligning to the leading of the stop button
+            resetButton.leadingAnchor.constraint(equalTo: stopButton.leadingAnchor),
+            resetButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 20)
+        ])
+    }
+    
+    // MARK: Rules Button
+    
+    func configureRulesButton(){
+        view.addSubview(rulesButton)
+        
+        NSLayoutConstraint.activate([
+            rulesButton.widthAnchor.constraint(equalToConstant: 115),
+            rulesButton.heightAnchor.constraint(equalToConstant: 50),
+            rulesButton.trailingAnchor.constraint(equalTo: stopButton.trailingAnchor),
+            rulesButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 20)
+        ])
+    }
+    
     
     
 
